078eca9090acdfc24adc3aec22f575d8f5758a38
Askhat Ashimov <ashimov.ashat05@gmail.com>
2023-05-25 18:54:19 +0600

    quiz page final

--- javascript-file.js
+++
@@ -1,5 +0,0 @@
-function random_character() {
-    let characters = ["Mark Zuckerberg", "Eduardo Saverin", "Sean Parker"]
-    let random = characters[Math.floor(Math.random() * 3)]
-    document.getElementById("character").innerHTML = "You are " + random;
-}
---
+++ scripts/quiz.js
@@ -0,0 +1,85 @@
+const quizData = [
+    {
+        question: "What university did zuckerberg attend?",
+        options: ["Harvard University", "Arizona College", "Oxford University", "Tokyo University"],
+        answer: "Harvard University"
+    },
+    {
+        question: "How old was Zuckerberg when he created Facebook?",
+        options: ["23", "20", "18", "26"],
+        answer: "20"
+    },
+    {
+        question: "What's an Mark's ex-girlfriend's name?",
+        options: ["Marylin Monroe", "Anna De Armas", "Erica Allbright", "Jessica Olsen"],
+        answer: "Erica Allbright"
+    },
+    {
+        question: "Who claims that Zuckerberg stole their idea?",
+        options: ["Bill Gates", "Joseph Joestar", "Winklevoss twins", "Dwayne Johnson"],
+        answer: "Winklevoss twins"
+    },
+    {
+        question: "Who is Zuckerberg's friend?",
+        options: ["Eduardo Saverin", "Keanu Reeves", "Harry Potter", "Jamal"],
+        answer: "Eduardo Saverin"
+    },
+    {
+        question: "Who is the author of the phrase \"Facebook me\"?",
+        options: ["Khalid Kashmiri", "Christy Lee", "Khidir Karawita", "Ismail Ahmad Kanabawi"],
+        answer: "Christy Lee"
+    }
+];
+
+var currentQuestion = 0;
+var score = 0;
+
+function displayQuestion() {
+    var question = quizData[currentQuestion];
+    document.getElementById("question-number").textContent = currentQuestion + 1 + "/6"
+    document.getElementById("question").textContent = question.question;
+
+    var optionsHTML = "";
+    question.options.forEach(function(option, index) {
+        optionsHTML += "<input class=\"form-check-input\" type='radio' name='answer' value='" + option + "' id='option" + index + "'>";
+        optionsHTML += "<label class=\"form-check-label\" for='option" + index + "'>" + option + "</label><br>";
+    });
+
+    document.getElementById("options").innerHTML = optionsHTML;
+}
+
+function checkAnswer() {
+    var selectedOption = document.querySelector("input[name='answer']:checked");
+    if (selectedOption) {
+        if (selectedOption.value === quizData[currentQuestion].answer) {
+            score++;
+        }
+        currentQuestion++;
+        if (currentQuestion < quizData.length) {
+            displayQuestion();
+        } else {
+            displayResult();
+        }
+    }
+}
+
+function displayResult() {
+    var quizContainer = document.getElementById("quiz-container");
+    quizContainer.innerHTML = "<h2>Quiz Complete!</h2>";
+    if (score >= 5) {
+        quizContainer.innerHTML += "<h3>Your score: " + score + "/" + quizData.length + "</h3>";
+        quizContainer.innerHTML += "<h3 style='font-weight: lighter'>Congratulations, you are very well versed in the movie</h3>";
+        quizContainer.innerHTML += "<img src='https://yt3.googleusercontent.com/cHfmUUnYTB44yIQYkqGrVB0bqN_EnuME4hJKZyP7kzIK7LQNyL-8XYzTKNlvvSB_boWHKiZGVQ=s900-c-k-c0x00ffffff-no-rj' style='width: 25%; height: 25%'>"
+    }
+    else if (score >= 3) {
+        quizContainer.innerHTML += "<h3>Your score: " + score + "/" + quizData.length + "</h3>";
+        quizContainer.innerHTML += "<h3 style='font-weight: lighter'>You know the basic information about the movie</h3>";
+        quizContainer.innerHTML += "<img src='https://pbs.twimg.com/media/D5Qkc2gWAAAD_mf.jpg:large' style='width: 25%; height: 25%'>"
+    }
+    else if (score < 3) {
+        quizContainer.innerHTML += "<h3>Your score: " + score + "/" + quizData.length + "</h3>";
+        quizContainer.innerHTML += "<h3 style='font-weight: lighter'>Have you really watched this movie?</h3>";
+        quizContainer.innerHTML += "<img src='images/bad.webp' style='width: 25%; height: 25%'>"
+
+    }
+}
