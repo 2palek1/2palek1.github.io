5bce837343107f769ba35e49439b8b72c52c4f12
Askhat Ashimov <ashimov.ashat05@gmail.com>
2023-06-23 20:08:07 +0600

    Quiz update

---
+++ scripts/questions.js
@@ -0,0 +1,53 @@
+// creating an array and passing the number, questions, options, and answers
+const questions = [
+  {
+    numb: 1,
+    question: 'What university did Zuckerberg attend??',
+    answer: 'Harvard University',
+    options: [
+      'Harvard University',
+      'Arizona College',
+      'Oxford University',
+      'Tokyo University',
+    ],
+  },
+  {
+    numb: 2,
+    question: "What's an Mark's ex-girlfriend's name?",
+    answer: 'Erica Allbright',
+    options: [
+      'Marylin Monroe',
+      'Anna De Armas',
+      'Erica Allbright',
+      'Jessica Olsen',
+    ],
+  },
+  {
+    numb: 3,
+    question: 'Who claims that Zuckerberg stole their idea?',
+    answer: 'Winklevoss twins',
+    options: [
+      'Bill Gates',
+      'Joseph Joestar',
+      'Winklevoss twins',
+      'Dwayne Johnson',
+    ],
+  },
+  {
+    numb: 4,
+    question: "Who is Zuckerberg's friend?",
+    answer: 'Eduardo Saverin',
+    options: ['Eduardo Saverin', 'Keanu Reeves', 'Harry Potter', 'Jamal'],
+  },
+  {
+    numb: 5,
+    question: 'Who is the author of the phrase "Facebook me"?',
+    answer: 'Christy Lee',
+    options: [
+      'Khalid Kashmiri',
+      'Christy Lee',
+      'Khidir Karawita',
+      'Ismail Ahmad Kanabawi',
+    ],
+  },
+];
